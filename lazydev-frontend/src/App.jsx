6bf6457b72fb178079a82d56cd2ac6ba207bf3d4
Johann-George <johanngeorge2003@gmail.com>
2025-10-17 10:20:16 +0530

    Setup tailwindcss

--- lazydev-frontend/src/App.jsx
+++ lazydev-frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { useNavigation } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const navigation = useNavigation();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      {navigation.state === "loading" ? (
+        <div className="flex items-center justify-center min-h-[852px]">
+          <span className="text-4xl font-semibold text-primary dark:text-light">
+            Loading...
+          </span>
+        </div>
+      ) : (
+        <Outlet />
+      )}
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- lazydev-frontend/src/main.jsx
+++ lazydev-frontend/src/main.jsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import { Bounce, ToastContainer } from 'react-toastify'
+import LandingPage from "./pages/LandingPage.jsx"
+import { RouterProvider } from 'react-router-dom'
+import ErrorPage from "./pages/ErrorPage.jsx"
+
+const routeDefinitions = createRoutesFromElements(
+  <Route path='/' element={<App/>} errorElement={<ErrorPage/>}>
+    <Route index element={<LandingPage/>}/>
+  </Route>
+)
+
+const appRouter = createBrowserRouter(routeDefinitions);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
+    <ToastContainer
+      position='top-center'
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      draggable
+      pauseOnHover
+      theme='light'
+      transition={Bounce}
+    />
   </StrictMode>,
 )
